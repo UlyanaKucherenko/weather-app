7d7e2f1ec9482e9726577c63957574c1fd1e26eb
UlyanaKucherenko <ulyanakucherenko28@gmail.com>
2023-02-08 10:49:41 +0200

    fixed height screen && built front

--- src/layouts/MainLayout/styled.ts
+++ src/layouts/MainLayout/styled.ts
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 const Wrap = styled.div`
   background-color: ${({ theme }) => theme.bg};
   color: ${({ theme }) => theme.text};
-  max-height: 100vh;
-  height: 100%;
+  height: 100vh;
 
   @media (max-width: 998px) {
-    max-height: 100%;
+    height: 100%;
   }
 `;
 export { Wrap };
